120943a3cc83b8bdf40eed76f17d7d658b72a747
Alaina Thompson <athompson9@hawk.iit.edu>
2022-01-21 13:12:12 -0600

    Initial Commit

---
+++ Shared/Bad_Version_HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  Bad_Version_HW1App.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Bad_Version_HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
