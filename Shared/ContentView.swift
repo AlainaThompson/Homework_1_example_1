23bb2f6f6a1751c953d885fe81e14d7d3c6f2418
Alaina Thompson <athompson9@hawk.iit.edu>
2022-01-21 13:30:51 -0600

    Bad_version

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Button("Calculate", action: {let _ = self.sphereVolume()})
+            .padding(.bottom)
             .padding()
     }
-}
 
+
+func sphereVolume() -> Double {
+    let radius = 2.0
+    let sphereVolume = (4/3)*Double.pi*pow(radius, 3)
+    print("Sphere of radius =", radius, "has a volume =", sphereVolume)
+    return sphereVolume
+    
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
 }
